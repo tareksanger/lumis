61fe39e22a07bec71f87b857d2d1cec2980eb63a
Tarek Sanger <tarek.sanger@me.com>
2024-11-02 11:46:02 -0400

    clean up simple memory

--- lumis/memory/simple_memory.py
+++ lumis/memory/simple_memory.py
@@ -4,14 +4,24 @@ from openai.types.chat import ChatCompletionMessageParam
 
 
 class SimpleMemory(BaseMemory):
-    def __init__(self):
+    def __init__(self, max_memory_size: int = 2000):
+        self.max_memory_size = max_memory_size
         self.__messages: list[ChatCompletionMessageParam] = []
 
     def add(self, message: ChatCompletionMessageParam):
         self.__messages.append(message)
 
     def get(self, *args, **kwargs) -> list[ChatCompletionMessageParam]:
-        return self.__messages
+        if len(self.__messages) <= self.max_memory_size:
+            return self.__messages
+
+        # Calculate the number of items to take from the start and the end
+        half_max = self.max_memory_size // 2
+
+        start = self.__messages[:half_max]
+        end = self.__messages[-half_max:]
+
+        return start + end
 
     def clear(self):
         self.__messages = []
@@ -21,9 +31,6 @@ class SimpleMemory(BaseMemory):
 
     def update(self, index: int, message: ChatCompletionMessageParam):
         self.__messages[index] = message
-
-    def insert(self, index: int, message: ChatCompletionMessageParam):
-        self.__messages.insert(index, message)
 
     @property
     def length(self) -> int:
